2f7d39315695ff245327a263042679b6a0076cad
Abdel Rahman AlSabbagh <92596599+AbdulSabb@users.noreply.github.com>
2022-07-28 14:43:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ from en.synonyms import get_synonyms_en
 from en.inflections import get_inflections_en
 from language_detector import detect_language
 
+from ar.auto_correct import get_correction_suggestions_ar
+from ar.synonyms import get_synonyms_ar
+from ar.inflections import get_inflections_ar
+from en.auto_correct import get_correction_suggestions_en
+from en.synonyms import get_synonyms_en
+from en.inflections import get_inflections_en
+from language_detector import detect_language
+
 
 def get_result(word):
     language = detect_language(word)
-    result = word + "\n"
+    result = {"exact": word}
 
     if language == "en":
-        result += get_correction_suggestions_en(word) + "\n"
-        result += get_synonyms_en(word) + "\n"
-        result += get_inflections_en(word) + "\n"
+        result["corrections"] = get_correction_suggestions_en(word)
+        result["synonyms"] = get_synonyms_en(word)
+        result["inflections"] = get_inflections_en(word)
+        result["lang"] = 'en'
 
     else:
-        result += get_correction_suggestions_ar(word) + "\n"
-        result += get_synonyms_ar(word) + "\n"
-        result += get_inflections_ar(word) + "\n"
+        result["corrections"] = get_correction_suggestions_ar(word)
+        result["synonyms"] = get_synonyms_ar(word)
+        result["inflections"] = get_inflections_ar(word)
+        result["lang"] = 'ar'
 
-    return result
+    return str(result)
+
 
 
 
